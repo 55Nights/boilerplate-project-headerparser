dea6fb182b34fc1cf2191fe8f30a3cf374b9a784
55Nights <1905587@students.kca.ac.ke>
2023-06-15 12:40:11 +0300

    whoami

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get('/api/hello', function (req, res) {
 app.get("/api/whoami", (req, res)=>{
   res.json({"ipaddress":req.ip,"language":req.headers['accept-language'],
 "software":req.headers['user-agent']})
-  console.log(req)
 })
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
